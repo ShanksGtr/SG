7bf86b01b343b844f9d1b5699dbee2cc4ecee051
Roronoa <wld-ice@hotamil.com>
2016-02-24 17:38:14 +0000

    2/22/2016

--- captcha.php
+++ captcha.php
@@ -3,7 +3,7 @@
     // Please acknowledge use of this code by including this header.
 
     // initialise image with dimensions of 120 x 30 pixels
-    $image = @imagecreatetruecolor(120, 30) or die("Cannot Initialize new GD image stream");
+    $image = @imagecreatetruecolor(160, 45) or die("Cannot Initialize new GD image stream");
 
     // set background and allocate drawing colours
     $background = imagecolorallocate($image, 0x66, 0xCC, 0xFF);
@@ -15,7 +15,7 @@
     // draw random lines on canvas
     for($i=0; $i < 8; $i++) {
         imagesetthickness($image, rand(1,3));
-        imageline($image, rand(0,120), 0, rand(0,120), 30 , $linecolor);
+        imageline($image, rand(0,160), 0, rand(0,160), 45 , $linecolor);
     }
 
     // using a mixture of system and GDF fonts
@@ -29,10 +29,10 @@
 
     // add random digits to canvas using random black/white colour
     $digit = '';
-    for($x = 15; $x <= 95; $x += 20) {
+    for($x = 10; $x <= 130; $x += 30) {
         $textcolor = (rand() % 2) ? $textcolor1 : $textcolor2;
         $digit .= ($num = rand(0, 9));
-        imagechar($image, array_pop($fonts), $x, rand(2, 14), $num, $textcolor);
+        imagettftext($image, 20, rand(-30,30), $x, rand(20, 42), $textcolor, $fonts[array_rand($fonts)], $num);
     }
 
     // record digits in session variable
