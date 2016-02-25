63409d904965ab647056fb24846729f80e50ddb7
1400003 <wld-ice@hotmail.com>
2016-02-25 16:22:27 +0000

    25/02/16

--- register.php
+++ register.php
@@ -128,7 +128,7 @@ if (isset($_POST['submit'])) {
                             <label class="col-sm-2 control-label">Terms and conditions</label>
                             <div class="col-sm-4" style="padding-top: 7px">
                                 <input type="checkbox" name="terms" id="terms"> <label for="terms">I agree on the <a href="terms.html">terms and conditions</a></label>
-                                <button id="agree" type="submit" name="submit" class="btn btn-default" value="send" style="margin-top: 15px">Sign up</button>
+                                <button id="agree" type="submit" name="submit" class="btn btn-default" value="send" style="margin-top: 10px">Sign up</button>
                                 <?php
                                 echo $newrecord;
                                 ?>
