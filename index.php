ee97027961d8b21a2e194204e68c381d0d8d0ee9
1400003 <wld-ice@hotmail.com>
2016-02-25 20:10:03 +0000

    25/02/16

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                 </div>
                 <div class="col-md-4"></div>
                 <div class="col-md-4">
-                    <a style="margin-bottom: 10px" href="about.php">About</a><br>
+                    <a style="padding-bottom: 10px" href="about.php">About</a><br>
                     <a href="terms.php">Terms</a>
                 </div>
             </div>
