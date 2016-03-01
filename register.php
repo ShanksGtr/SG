b6552b6b5a5bce4239fd5095bd769ddac2fff2e1
1400003 <wld-ice@hotmail.com>
2016-03-01 13:48:27 +0000

    25/02/16

--- register.php
+++ register.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit'])) {
     $sqlinsert = "INSERT INTO users (user_name, email, password, reg_date) VALUES
             ('$username', '$email', '$password', '$reg_date')";
     if (!mysqli_query($db, $sqlinsert)) {
-        echo ("<script>alert('Username or Email has been used already');</script>");
+        echo ("<script>alert('Username or Email has been used already'); location.href='Register.php';</script>");
     } else {
      echo "<script>alert('You are successfully registered'); location.href='login.php';</script>";
 }}
