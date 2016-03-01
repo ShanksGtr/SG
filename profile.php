bcca1c60966c07491e8271f036f7a1563d6ff80a
1400003 <wld-ice@hotmail.com>
2016-03-01 15:38:46 +0000

    25/02/16

--- profile.php
+++ profile.php
@@ -89,7 +89,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                     <?php } else { ?>
                         <h1>Welcome to SGamers</h1> <?php } ?>
                 </div>
-                <div class="row">
+                <div class="row-fluid">
                     <div class="col-md-8">
                         <p>djafjdpoiajpfidjmpfijmapsdjmpifjmapsjmdpfjajsfapiajspoifjapisjfmpoiajmspofiajfpoijaijfpiajspimfjmsapifjpiajmspifjmpiajmspifjapisjmfpiajmspifjmapisjmfpisajmpifsaj</p>
                     </div>
