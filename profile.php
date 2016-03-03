ca31bb9ea81c418e9a3b468284db10c85250fad0
1400003 <wld-ice@hotmail.com>
2016-03-03 20:40:55 +0000

    03/03/2016

--- profile.php
+++ profile.php
@@ -125,7 +125,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                         <div class="col col-md-9" style="word-wrap: break-word">
                             <div>
                                 <?php if ($_SESSION['username'] == true) { ?>
-                                <button class="btn btn-default btn-lg" style="float: right; margin-top: -25px; font-family: 'Press Start 2P', cursive;" type="submit">Edit</button>
+                                <a class="btn btn-default btn-lg" style="float: right; margin-top: -25px; font-family: 'Press Start 2P', cursive;" href="profile.php?edit" type="submit">Edit</a>
                                 <?php } else {
                                 }?>
                                 <h2>Status:</h2>
