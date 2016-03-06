459eeb853197412fa34a24bfdd3522ad572cbf31
Roronoa <wld-ice@hotamil.com>
2016-03-06 23:22:28 +0000

    06/03/2016

--- PHP/profiling.php
+++ PHP/profiling.php
@@ -36,7 +36,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
         $run_query = $db->query($list_query);
         $check_user = mysqli_num_rows($run_query);
             if ($check_user == 1) {
-                $list_query = "UPDATE profiles SET  WHERE user_name ='{$_SESSION['userid']}'";
+                //$list_query = "UPDATE profiles SET  WHERE user_name ='{$_SESSION['userid']}'";
                 echo "<script>alert('COOL'); location.href='/profile.php';</script>";
 
             } elseif ($check_user == 0) {
