5a2be81b8ec98a1eb86f350b115dc640454872d6
Roronoa <wld-ice@hotamil.com>
2016-03-07 17:42:35 +0000

    06/03/2016

--- profiledit.php
+++ profiledit.php
@@ -114,7 +114,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                                 echo "<p>" . "<input required=required type=number max=150 class=form-control placeholder=YourAge name=age " . "</p>";
                                  } else { ?>
                                 <p><input required="required" type="number" max="150" class="form-control" placeholder="Your age" name="age" value="<?= $row['age']; ?>"> </p>
-                                <?php echo $row['age']; }} ?>
+                                <?php }} ?>
                                 <h3><span class="fi-torsos-male-female"></span> Gender:</h3>
                                 <h3><input type="radio" name="gender" value="Male"> Male
                                     <input type="radio" name="gender" value="Female"> Female <br>
