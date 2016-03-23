81c9cbe153f40e2cfad596f2ba3ea76b976a225d
Roronoa <wld-ice@hotamil.com>
2016-03-23 19:01:09 +0000

    03/23/2016

--- upcoming.php
+++ upcoming.php
@@ -193,16 +193,15 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                             ?>
 
             </div>
-        <div hidden>
-            <?php
-
-            $html = file_get_html('http://www.videogamecountdown.com/');
-            foreach($html->find('body script') as $body)
-                echo $body->outertext;
-            ?>
         </div>
-        </div>
+    <div hidden>
+        <?php
 
+        $html = file_get_html('http://www.videogamecountdown.com/');
+        foreach($html->find('body script') as $body)
+            echo $body->outertext;
+        ?>
+    </div>
     </div>
 </div>
 
