2ecaa03cddd02e22c303c922c225e105c6744ff5
Roronoa <wld-ice@hotamil.com>
2016-03-23 18:59:26 +0000

    03/23/2016

--- upcoming.php
+++ upcoming.php
@@ -92,15 +92,8 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                 <h1>Welcome to SGamers</h1> <?php } ?>
         </div>
         <div style="word-wrap: break-word">
-            <div hidden>
                 <?php
                 include('simple_html_dom.php');
-                $html = file_get_html('http://www.videogamecountdown.com/');
-                foreach($html->find('body script') as $body)
-                    echo $body->outertext;
-                ?>
-            </div>
-                <?php
                     if ($_GET['plat'] == "ps3") {
                     $html = file_get_html('http://www.videogamecountdown.com/coming-soon/PlayStation-3');
                     $games = $html->find('div[class=inner]');
@@ -198,7 +191,14 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                                 }
 
                             ?>
+            <div hidden>
+                <?php
 
+                $html = file_get_html('http://www.videogamecountdown.com/');
+                foreach($html->find('body script') as $body)
+                    echo $body->outertext;
+                ?>
+            </div>
             </div>
         </div>
 
