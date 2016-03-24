4940b3f3ef6e6e5bf5fc81c2c7efa75dfa564e00
1400003 <wld-ice@hotmail.com>
2016-03-24 20:18:17 +0000

    24/03/2016

--- index.php
+++ index.php
@@ -67,6 +67,23 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                             </ul>
                         </li>
                     </ul>
+                        <?php if ($_SESSION['username'] == true) { ?>
+                            <li class="dropdown navbar-right">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"><?php echo substr($_SESSION['username'], 0 , 9);?></span></a>
+                                <ul class="dropdown-menu">
+                                     <li><a href="profile.php"><span class="glyphicon glyphicon-edit" style="font-size:20px; margin-right: 2px"></span>MyProfile</a></li>
+                                     <li><a href="PHP/logout.php"><span class="glyphicon glyphicon-log-out" style="font-size:20px; margin-right: 3px"></span>Logoff</a></li>
+                                </ul>
+                            </li>
+                            <?php } else { ?>
+                                <li class="dropdown navbar-right">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in">Sign In/Up</span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="login.php"><span class="glyphicon glyphicon-log-in" style="font-size:20px; margin-right: 3px"></span>SignIn</a></li>
+                                        <li><a href="register.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>SignUp</a></li>
+                                    </ul>
+                                </li>
+                            <?php } ?>
                     <ul class="nav navbar-nav navbar-right">
                         <?php if ($_SESSION['username'] == true) { ?>
                             <li><a href="PHP/logout.php"><span class="glyphicon glyphicon-log-out" style="font-size:20px; margin-right: 3px"></span>Logoff</a></li>
@@ -76,7 +93,6 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                             <li><a href="register.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>SignUp</a></li>
                         <?php } ?>
                     </ul>
-
                 </div>
             </div>
             <div class="container-fluid"></div>
