2fbffd4f10c2c17e01962918a6470cef1eeca412
1400003 <wld-ice@hotmail.com>
2016-03-24 16:31:33 +0000

    24/03/2016

--- upcoming.php
+++ upcoming.php
@@ -91,7 +91,6 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
             <?php } else { ?>
                 <h1>Welcome to SGamers</h1> <?php } ?>
         </div>
-        <div style="word-wrap: break-word">
                 <?php
                 include('simple_html_dom.php');
                     if ($_GET['plat'] == "ps3") {
@@ -99,7 +98,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                     $games = $html->find('div[class=inner]');
                     $games = array($games[0], $games[1], $games[3], $games[4], $games[5]);
                     foreach ($games as $game) {
-
+                        echo '<div class="row">';
                         $titles = $game->find('h3 a', 0)->plaintext;
                         $images = $game->find('div[class=gridimg] img',0)->attr['src'];
                         $info = $game->find('div[class=gridimg] a',0)->attr['href'];
@@ -112,7 +111,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
 
                         echo '<div class="col col-md-6">' .  "<h2>".$titles."</h2>" . '<img src="'.$images.'"/>' . '</div>' .
                             '<div class="col col-md-6">' . $desc ."<br>" . "<p>Upcoming in: ".$date."</p>" . "<br>" . $details . "<br>" . $amazon . '</div>';
-                    }
+                    }   echo '</div>';
 
                         } elseif ($_GET['plat'] == "ps4") {
                             $html = file_get_html('http://www.videogamecountdown.com/coming-soon/PlayStation-4');
