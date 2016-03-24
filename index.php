dcc074c3152f660ef589e4dc798abc75fc64a417
1400003 <wld-ice@hotmail.com>
2016-03-24 16:18:23 +0000

    24/03/2016

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
 
     </script>
     <!-- For Mobiles -->
-     <meta name="viewport" content="width=device-width, initial-scale=0.8 maximum-scale=1, user-scalable=yes">
+     <meta name="viewport" content="width=device-width, initial-scale=0.5 maximum-scale=1, user-scalable=yes">
     <meta charset="UTF-8">
     <title>SGamers</title>
     <!-- Bootstrap CSS,JQ&JS Libraries-->
