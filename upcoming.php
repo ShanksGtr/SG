0421136caf3e77c646f3f02bf82e5d7f603c5808
1400003 <wld-ice@hotmail.com>
2016-03-24 16:32:59 +0000

    24/03/2016

--- upcoming.php
+++ upcoming.php
@@ -118,7 +118,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                             $games = $html->find('div[class=inner]');
                             $games = array($games[0], $games[1], $games[3], $games[4], $games[5]);
                             foreach ($games as $game) {
-
+                                echo '<div class="row">';
                                 $titles = $game->find('h3 a', 0)->plaintext;
                                 $images = $game->find('div[class=gridimg] img', 0)->attr['src'];
                                 $info = $game->find('div[class=gridimg] a', 0)->attr['href'];
@@ -131,8 +131,8 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
 
                                 echo '<div class="col col-md-6 colum1">' . "<h2>" . $titles . "</h2>" . '<img src="' . $images . '"/>' . '</div>' .
                                     '<div class="col col-md-6">' . $desc . "<br>" . "<p>Upcoming in: " . $date . "</p>" . "<br>" . $details . "<br>" . $amazon . '</div>';
-
-                         }} elseif ($_GET['plat'] == "xbox") {
+                            } echo '</div>';
+                         } elseif ($_GET['plat'] == "xbox") {
                         $html = file_get_html('http://www.videogamecountdown.com/coming-soon/Xbox-One');
                         $games = $html->find('div[class=inner]');
                         $games = array($games[0], $games[1], $games[3], $games[4], $games[5]);
