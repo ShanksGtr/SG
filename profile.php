90bd25e45c63df26005aa0ec2285f93cdf22720f
1400003 <wld-ice@hotmail.com>
2016-03-25 17:03:05 +0000

    25/03/2016

--- profile.php
+++ profile.php
@@ -75,11 +75,14 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <?php if ($_SESSION['username'] == true) { ?>
-                            <li><a href="PHP/logout.php"><span class="glyphicon glyphicon-log-out" style="font-size:20px; margin-right: 3px"></span>Logoff</a></li>
-                            <li><a href="profile.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>MyProfile</a></li>
-                        <?php } else { ?>
-                            <li><a href="login.php"><span class="glyphicon glyphicon-log-in" style="font-size:20px; margin-right: 3px"></span>SignIn</a></li>
-                            <li><a href="register.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>SignUp</a></li>
+                            <li class="active dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" style="font-size: 20px;"></span><?php echo substr($_SESSION['username'], 0 , 9);?></a>
+                                <ul class="dropdown-menu">
+                                    <li class="active"><a href="profile.php"><span class="glyphicon glyphicon-edit" style="font-size:20px; margin-right: 2px"></span>MyProfile</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="PHP/logout.php"><span class="glyphicon glyphicon-log-out" style="font-size:20px; margin-right: 3px"></span>Logoff</a></li>
+                                </ul>
+                            </li>
                         <?php } ?>
                     </ul>
                 </div>
--- terms.php
+++ terms.php
@@ -70,15 +70,25 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php if ($_SESSION['username'] == true) { ?>
-                    <li><a href="PHP/logout.php"><span class="glyphicon glyphicon-log-out" style="font-size:20px; margin-right: 3px"></span>Logoff</a></li>
-                    <li><a href="profile.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>MyProfile</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" style="font-size: 20px;"></span><?php echo substr($_SESSION['username'], 0 , 9);?></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="profile.php"><span class="glyphicon glyphicon-edit" style="font-size:20px; margin-right: 2px"></span>MyProfile</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="PHP/logout.php"><span class="glyphicon glyphicon-log-out" style="font-size:20px; margin-right: 3px"></span>Logoff</a></li>
+                        </ul>
+                    </li>
                 <?php } else { ?>
-                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in" style="font-size:20px; margin-right: 3px"></span>SignIn</a></li>
-                    <li><a href="register.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>SignUp</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in" style="margin-right: 7px; font-size: 20px;"></span>SignIn/Up</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="login.php"><span class="glyphicon glyphicon-log-in" style="font-size:20px; margin-right: 3px"></span>SignIn</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="register.php"><span class="glyphicon glyphicon-user" style="font-size:20px; margin-right: 2px"></span>SignUp</a></li>
+                        </ul>
+                    </li>
                 <?php } ?>
-
             </ul>
-
         </div>
     </div>
     <div class="container-fluid"></div>
