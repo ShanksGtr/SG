40597e5b56c6b6c5993f60088fdd3d1506449e3e
1400003 <wld-ice@hotmail.com>
2016-03-29 16:10:25 +0100

    29/03/2016

--- index.php
+++ index.php
@@ -3,24 +3,6 @@ session_start();
 if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
 }
 
-include('simple_html_dom.php');
-$html = file_get_html('http://www.videogamecountdown.com/');
-$games = $html->find('div[class=inner]');
-
-$games= array($games[0], $games[1], $games[3], $games[4], $games[5]);
-foreach ($games as $game) {
-
-
-$titles = $game->find('h3 a', 0)->plaintext;
-$images = $game->find('div[class=gridimg] img', 0)->attr['src'];
-$info = $game->find('div[class=gridimg] a', 0)->attr['href'];
-$date = $game->find('div[class=date] span', 0)->outertext;
-$html = file_get_html('http://www.videogamecountdown.com/' . $info);
-$desc = $html->find('div[class=two_third]', 0)->innertext;
-$details = $html->find('div[class="one_third last projectdetails"]', 0)->outertext;
-$amazon = $html->find('div[class=pagerwrapper] a', -1)->outertext;
-//$titles = $game->find('h3', 0);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -125,11 +107,29 @@ $amazon = $html->find('div[class=pagerwrapper] a', -1)->outertext;
                 <div class="rslides">
                     <!--  https://www.youtube.com/watch?v=MwTm53hpzi8 && http://responsiveslides.com/themes/themes.html -->
                     <?php
+                    include('simple_html_dom.php');
+                    $html = file_get_html('http://www.videogamecountdown.com/');
+                    $games = $html->find('div[class=inner]');
+
+                    $games= array($games[0], $games[1], $games[3], $games[4], $games[5]);
+                    foreach ($games as $game) {
+
+
+                        $titles = $game->find('h3 a', 0)->plaintext;
+                        $images = $game->find('div[class=gridimg] img', 0)->attr['src'];
+                        $info = $game->find('div[class=gridimg] a', 0)->attr['href'];
+                        $date = $game->find('div[class=date] span', 0)->outertext;
+                        $html = file_get_html('http://www.videogamecountdown.com/' . $info);
+                        $desc = $html->find('div[class=two_third]', 0)->innertext;
+                        $details = $html->find('div[class="one_third last projectdetails"]', 0)->outertext;
+                        $amazon = $html->find('div[class=pagerwrapper] a', -1)->outertext;
+                        //$titles = $game->find('h3', 0);
+
                         echo '<li>';
-                    echo '<div class="row textglow ">' . '<div class="col col-md-6">' . "<h2>" . $titles . "</h2>" . '<img src="' . $images . '"/>' . '</div>' .
+                        echo '<div class="row textglow ">' . '<div class="col col-md-6">' . "<h2>" . $titles . "</h2>" . '<img src="' . $images . '"/>' . '</div>' .
                         '<div class="col col-md-6">' . $desc . "<br>" . "<p>Upcoming in: " . $date . "</p>" . "<br>" . $details . "<br>" . $amazon . '</div>' . '</div>';
                     }
-                    echo '</li>';
+                        echo '</li>';
                     ?>
                 </div>
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
