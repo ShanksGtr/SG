85dd9b8df477d13d74075973262cc9c8301931b8
1400003 <wld-ice@hotmail.com>
2016-03-29 16:00:28 +0100

    29/03/2016

--- index.php
+++ index.php
@@ -177,6 +177,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                 </div>
             </div>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
