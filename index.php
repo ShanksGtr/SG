3decf8cc385fcb54dffed3cff4ca7bd852ed7387
1400003 <wld-ice@hotmail.com>
2016-03-29 17:34:20 +0100

    29/03/2016

--- index.php
+++ index.php
@@ -103,7 +103,6 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                      <?php } else { ?>
                      <h1>Welcome to SGamers</h1> <?php } ?>
                 </div>
-                <div class="row">
                 <div class="rslides">
                     <!--  https://www.youtube.com/watch?v=MwTm53hpzi8 && http://responsiveslides.com/themes/themes.html -->
                     <?php
@@ -157,7 +156,6 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
                     });
                 </script>
                 </div>
-            </div>
         </div>
         <div class="footer">
             <div class="container-fluid" style="height: 2px"></div>
