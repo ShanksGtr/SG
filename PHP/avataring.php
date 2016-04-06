89bd2fb240fb4cb5d22757af9c1f62d73d3bc0c4
Roronoa <wld-ice@hotamil.com>
2016-04-06 03:09:58 +0100

    4/5/2016

--- PHP/avataring.php
+++ PHP/avataring.php
@@ -3,14 +3,12 @@ session_start();
 if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
     echo "<script>alert('Please Login'); location.href='login.php';</script>";
 }
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    require('config.php');
+
+
 
 
 // https://www.youtube.com/watch?v=wEmxwNLjf_c  && http://www.w3schools.com/php/php_file_upload.asp
-if(isset($_FILES['fileToUpload'])){
+/*if(isset($_FILES['fileToUpload'])){
 
     $uploadname = $_FILES['fileToUpload']['name'];
     $uploadname = mt_rand(10000, 99999).$uploadname;
@@ -34,6 +32,11 @@ if(isset($_FILES['fileToUpload'])){
         echo "<script>alert('No file selected'); location.href='/profile.php';</script>";
 
     }else{
+        require('config.php');
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
         move_uploaded_file($uploadtmp, "" . $uploadname);
         $userid = $_SESSION['userid'];
         $sqlinsert= "UPDATE profiles SET avatar='$uploadname' WHERE user_id='$userid'";
@@ -43,4 +46,23 @@ if(isset($_FILES['fileToUpload'])){
 
 
     }
-}
+}*/
+
+$target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            echo $imageFileType . "<br>";
+            echo $target_file  . "<br>";
+            echo $target_dir  . "<br>";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
