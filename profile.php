6b76561d1309ffc9bb682c5b03fd32b6101bd3bb
Roronoa <wld-ice@hotamil.com>
2016-04-06 01:20:51 +0100

    4/5/2016

--- profile.php
+++ profile.php
@@ -4,41 +4,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
     echo "<script>alert('Please Login'); location.href='/login.php';</script>";
 }
 
-// https://www.youtube.com/watch?v=wEmxwNLjf_c  && http://www.w3schools.com/php/php_file_upload.asp
-/*if(isset($_FILES['fileToUpload'])){
 
-    $uploadname = $_FILES['fileToUpload']['name'];
-    $uploadname = mt_rand(10000, 99999).$uploadname;
-    $uploadtmp = $_FILES['fileToUpload']['tmp_name'];
-    $uploadtype = $_FILES['fileToUpload']['type'];
-    $filesize = $_FILES['fileToUpload']['size'];
-    // incase there's a space in the name or so
-    $uploadname = preg_replace("#[^a-z0-9.]#i", "", $uploadname);
-
-    $imageFileType = pathinfo($uploadname,PATHINFO_EXTENSION);
-
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-        echo "<script>alert('Sorry, only JPG, JPEG & PNG  files are allowed'); location.href='/profile.php';</script>";
-        }
-
-    if(($filesize > 1000000)) {
-        echo "<script>alert('File is more than 1mb'); location.href='/profile.php';</script>";
-    }
-
-    if(!$uploadtmp) {
-        echo "<script>alert('No file selected'); location.href='/profile.php';</script>";
-
-    }else{
-        move_uploaded_file($uploadtmp, "" . $uploadname);
-        $user_id = $_SESSION['userid'];
-        $sqlinsert= "INSERT INTO profiles (avatar) VALUES ('$uploadname') WHERE user_id='$user_id'";
-        $result = mysqli_query($db, $sqlinsert);
-        echo "<script>alert('Upload successfully'); location.href='profile.php';</script>";
-        //echo  '<img src="'.$uploadname.'"/>' . "<br>";
-
-
-    }
-}*/
 ?>
 <!DOCTYPE html>
     <html lang="en">
