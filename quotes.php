35a931921923da33a7fca8d8382dc74632297c14
1400003 <wld-ice@hotmail.com>
2016-04-15 17:28:37 +0100

    15/4

--- quotes.php
+++ quotes.php
@@ -119,10 +119,13 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
             </div>
         </div>
         <div>
+            <fieldset>
+                <legend>Metal Gear</legend>
             <blockquote>
                 7o6 al7sh-h fel shrb6h tlga alan6rah fee alsustah agataga ahooondagaaa zellolol shereureur bahhuhuhuhu beloololoolol lalaallalala shahahahahaha fosofosfosfosfs
                 <cite>Khaled Sami</cite>
             </blockquote>
+                </fieldset>
         </div>
     </div>
 </div>
