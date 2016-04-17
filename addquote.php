4635f9c6a803e551768fc6ed9c385a66475feabc
Roronoa <wld-ice@hotamil.com>
2016-04-17 01:53:16 +0100

    4/16

--- addquote.php
+++ addquote.php
@@ -9,7 +9,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
         include('$db');
         $q_name = $_POST['charname'];
         $q_game = $_POST['gamename'];
-        $quote = $_POST['quote'];
+        $quote = mysqli_real_escape_string($_POST['quote']);
         $quote = htmlspecialchars($quote);
         $user_id = $_SESSION['userid'];
 
