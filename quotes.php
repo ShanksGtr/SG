5cb34036631dd0cd16fd170c13fb206cbd20202c
Roronoa <wld-ice@hotamil.com>
2016-04-18 01:57:07 +0100

    4/16

--- quotes.php
+++ quotes.php
@@ -120,6 +120,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
         </div>
         <div>
             <?php
+                // Got pagination from:  https://www.youtube.com/watch?v=T2QFNu_mivw
                 require('PHP/config.php');
                 ini_set('display_errors', 1);
                 ini_set('display_startup_errors', 1);
@@ -204,7 +205,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
         </div>
     </div>
 </div>
-<div class="footer" style="position: fixed">
+<div class="footer" style="position: fixed; @media all and (max-width: 699px) and (min-width: 100px) { .footer{position: absolute;}}">
     <div class="container-fluid" style="height: 2px"></div>
     <div class=container>
         <div class="col-md-4">
