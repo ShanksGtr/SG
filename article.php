7f2f73d444bd4e47a8d3155ef74c89dbb89b0e67
Roronoa <wld-ice@hotamil.com>
2016-04-30 17:40:13 +0100

    Last day

--- article.php
+++ article.php
@@ -137,7 +137,7 @@ if($_GET['art'] == NULL){
         $a_article = $row['a_text'];
         ?>
         <div class="page-header textglow2">
-            <h2>Article: <?= $a_title ?></h2>
+            <h1>Article: <?= $a_title ?></h1>
         </div>
         <div>
 
