d7edd8cde05ed321efa3fb84168582115590934a
Roronoa <wld-ice@hotamil.com>
2016-05-01 17:57:12 +0100

    Last day

--- PHP/conf.php
+++ PHP/conf.php
@@ -2,6 +2,14 @@
 
 if(isset($_POST['user'])){
     echo $_POST['user'];
+
+} elseif ($_POST['art']) {
+    echo $_POST['art'];
+
+} elseif ($_POST['quote']) {
+    echo $_POST['quote'];
+
 } else {
     header('Location:/index');
 }
+
