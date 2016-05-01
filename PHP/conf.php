46fe75995c9c9862204f3c0f3056b6914eb99897
Roronoa <wld-ice@hotamil.com>
2016-05-01 19:03:19 +0100

    Last day

--- PHP/conf.php
+++ PHP/conf.php
@@ -1,15 +1,31 @@
+<script>
+    var conf = confirm("Are you sure you want to delete?");
+    if (conf){
+
 <?php
 
 if(isset($_POST['user'])){
-    echo $_POST['user'];
 
+    echo "Yes";
+   // //$user_id = $_POST['user'];
+  //  $del = "DELETE FROM users WHERE user_id='$user_id'";
+   // $run = $db->query($del);
+   // if ($run){
+
+  //  }
 } elseif ($_POST['art']) {
-    echo $_POST['art'];
+    $art_id = $_POST['art'];
+
 
 } elseif ($_POST['quote']) {
-    echo $_POST['quote'];
+    $q_id = $_POST['quote'];
+
 
 } else {
     header('Location:/index');
 }
-
+?>
+        } else {
+                location.href='adp.php';
+            }
+</script>
