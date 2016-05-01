0f991f6461f57db85040ed2f92c0b6f94234e51d
Roronoa <wld-ice@hotamil.com>
2016-05-01 19:40:52 +0100

    Last day

--- PHP/conf.php
+++ PHP/conf.php
@@ -20,7 +20,7 @@ if(isset($_POST['user'])){
 
 } elseif ($_POST['art']) {
     $art_id = $_POST['art'];
-    $del = "DELETE FROM articles WHERE user_id='$art_id'";
+    $del = "DELETE FROM articles WHERE a_id='$art_id'";
     $run = $db->query($del);
     if ($run){
         echo "<script>alert('Article Deleted'); location.href='adp.php';</script>";
@@ -30,7 +30,7 @@ if(isset($_POST['user'])){
 
 } elseif ($_POST['quote']) {
     $q_id = $_POST['quote'];
-    $del = "DELETE FROM quotes WHERE user_id='$q_id'";
+    $del = "DELETE FROM quotes WHERE q_id='$q_id'";
     $run = $db->query($del);
     if ($run){
         echo "<script>alert('Quote Deleted'); location.href='adp.php';</script>";
